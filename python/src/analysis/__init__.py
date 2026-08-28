ae9c77df1f6e3a497ecb0addf2243d3078f10528
José Grilo <jose.grilo@research.fchampalimaud.org>
2026-08-28 11:25:23 +0100

    fix: deal with missing data files

--- python/src/analysis/__init__.py
+++ python/src/analysis/__init__.py
@@ -12,7 +12,12 @@ def main() -> None:
         if dir.is_dir():
             reader = harp.create_reader(dir)
 
-            data = reader.CapacitanceValues.read(dir / "FlyPAD_33.bin")
-            # data = data.rename(columns=dict(zip(data.columns.tolist(), COLUMN_NAMES)))
-            data_arr = data.values >> 4
-            data_arr.reshape(-1).astype("ushort").tofile(dir / "FlyPAD_33_12bits.bin")
+            try:
+                data = reader.CapacitanceValues.read(dir / "FlyPAD_33.bin")
+                # data = data.rename(columns=dict(zip(data.columns.tolist(), COLUMN_NAMES)))
+                data_arr = data.values >> 4
+                data_arr.reshape(-1).astype("ushort").tofile(
+                    dir / "FlyPAD_33_12bits.bin"
+                )
+            except:
+                print(f"{dir / 'FlyPAD_33.bin'} does not exist")
